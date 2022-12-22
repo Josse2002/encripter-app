7a5f35bdaf842db54b87ff6c20f060cc15fc13ee
Josse2002 <jcastillo10b@gmail.com>
2022-12-22 10:35:57 -0600

    :memo: decrypt function

--- js/main.js
+++ js/main.js
@@ -14,9 +14,9 @@ function copyToClipboard() {
     var copyText = encryptedOutput.innerText;
     navigator.clipboard.writeText(copyText).then(() => {
         alert("Texto copiado al portapapeles");
-      }, () => {
+    }, () => {
         alert("No se pudo copiar el texto al portapapeles");
-      });
+    });
 
 }
 
@@ -72,8 +72,36 @@ function encrypt() {
     textFound.style.display = "flex";
     notFoundText.style.display = "none";
     encryptedOutput.innerHTML = output;
+    encryptedInput.value = "";
 }
+
+//Función para desencriptar
+function decrypt() {
+    var input = encryptedInput.value;
+    var letters = ["a", "e", "i", "o", "u"];
+    //Este input se correra las 6 veces de las letras
+    for (let i = 0; i < 6; i++) {
+        //Mientras este otro, verifica en cada una de las palabras del input
+        for (let j = 0; j < input.length; j++) {
+            //Si el input incluye alguna de las palabras del arreglo, entonces
+            if (input.includes(encryptedConverter[i])) {
+                //Reemplaza la palabra del arreglo por la letra correspondiente
+                input = input.replace(encryptedConverter[i], letters[i]);
+            }
+
+        }
+
+    }
+    
+    textFound.style.display = "flex";
+    notFoundText.style.display = "none";
+    encryptedOutput.innerHTML = input;
+    encryptedInput.value = "";
+
+}
+
 
 encryptButton.addEventListener("click", encrypt);
 encryptedInput.addEventListener("keyup", normalizeText);
+decryptButton.addEventListener("click", decrypt);
 copyButton.addEventListener("click", copyToClipboard);
